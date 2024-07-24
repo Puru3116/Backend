4538e8aa1374a78220d044ccf6370d1472947971
Piyush <119452340+Puru3116@users.noreply.github.com>
2024-07-24 22:19:11 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 // Apply CORS middleware first
 app.use(cors({
-  origin: 'https://frontend-blogging.vercel.app',
+  origin: 'https://frontend-git-main-piyushs-projects-0a22a4d7.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
