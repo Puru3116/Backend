378952df8f7088f22507415dfbd12fd7b52b1ada
Piyush <119452340+Puru3116@users.noreply.github.com>
2024-07-24 21:59:04 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,6 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
     'https://loquacious-seahorse-614b70.netlify.app',
-    'https://frontend-blogging.vercel.app'
     'https://frontend-beige-rho.vercel.app'
 ];
 
